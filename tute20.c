0bb0e8a5b674eb14e6c871eba1d2fb1b9cd1afa3
Neha Patel <nehapatel8115291774@gmail.com>
2026-05-25 19:56:18 +0530

    first upload

---
+++ tute20.c
@@ -0,0 +1,28 @@
+//Wap that simply takes elementof the array from he user and find the sum of these element.
+#include<stdio.h>
+int main()
+{int n,A[50], i,sum=0;
+    printf("enter number of element ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++);
+    {
+    printf("enter the array element ");
+    scanf("%d",A[i]);
+    }
+
+    sum=sum+A[i];
+
+printf("sum=%d", sum);
+
+}
+    
+
+
+
+
+
+
+
+
+
